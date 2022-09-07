a8edede35a2aeb98db7c783ad9b54a269261c19d
Sebastian Rojas <sebosinaga@gmail.com>
2022-09-07 08:08:36 -0400

    heroku config:

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ const routes = require('./routes');
 const app = express();
 const cors = require('cors');
 
+app.set('port', (process.env.PORT))
+
 app.use(cors());
 
 app.use(express.json());
 app.use('/api', routes);
 
-app.listen(5000, () => {
-    
+app.listen(app.get('port'), () => {
     console.log('Happy Codding the app listening on port 5000!');
 });
